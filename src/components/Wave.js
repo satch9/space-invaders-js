b9acb2ec0d559e1e19f82d2ee8b6a4d68d01e780
Vincent Jue <viny1976@gmail.com>
2024-07-02 03:46:39 +0000

    2eme partie de wave, rajout assets ennemie et vaisseau du joueur

--- src/components/Wave.js
+++ src/components/Wave.js
@@ -1,23 +1,38 @@
 class Wave {
   constructor(game) {
-    this.game = game;
-    this.width = this.game.columns * this.game.enemySize;
-    this.height = this.game.rows * this.game.enemySize;
-    this.x = 0;
-    this.y = 0;
-    this.speedX = 3;
-    this.speedY = 0;
-    this.enemies = [];
+    this.game = game
+    this.width = this.game.columns * this.game.enemySize
+    this.height = this.game.rows * this.game.enemySize
+    this.x = 0
+    this.y = -this.height
+    this.speedX = 3
+    this.speedY = 0
+    this.enemies = []
+    this.create()
   }
   render(context) {
-    this.speedY = 0;
-    context.strokeRect(this.x, this.y, this.width, this.height);
+    if (this.y < 0) this.y += 5
+    this.speedY = 0
+    //context.strokeRect(this.x, this.y, this.width, this.height)
 
     if (this.x < 0 || this.x > this.game.width - this.width) {
-      this.speedX *= -1;
-      this.speedY = this.game.enemySize;
+      this.speedX *= -1
+      this.speedY = this.game.enemySize
     }
-    this.x += this.speedX;
-    this.y += this.speedY;
+    this.x += this.speedX
+    this.y += this.speedY
+    this.enemies.forEach((enemy) => {
+      enemy.update(this.x, this.y)
+      enemy.draw(context)
+    })
+  }
+  create() {
+    for (let y = 0; y < this.game.rows; y++) {
+      for (let x = 0; x < this.game.columns; x++) {
+        let enemyX = x * this.game.enemySize
+        let enemyY = y * this.game.enemySize
+        this.enemies.push(new Enemy(this.game, enemyX, enemyY))
+      }
+    }
   }
 }
