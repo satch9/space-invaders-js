9bd8152e4dbcc70a11674927b757723580e90b98
satch9 <viny1976@gmail.com>
2024-07-03 11:40:24 +0400

    rajout de la classe enfant beetlemorph de la classe parent enemy

---
+++ src/components/Beetlemorph.js
@@ -0,0 +1,9 @@
+class Beetlemorph extends Enemy {
+  constructor(game, positionX, positionY) {
+    super(game, positionX, positionY);
+    this.image = document.getElementById("beetlemorph");
+  }
+
+  draw() {}
+  update() {}
+}
--- src/components/Wave.js
+++ src/components/Wave.js
@@ -1,39 +1,39 @@
 class Wave {
   constructor(game) {
-    this.game = game
-    this.width = this.game.columns * this.game.enemySize
-    this.height = this.game.rows * this.game.enemySize
-    this.x = 0
-    this.y = -this.height
-    this.speedX = 3
-    this.speedY = 0
-    this.enemies = []
-    this.nextWaveTrigger = false
-    this.create()
+    this.game = game;
+    this.width = this.game.columns * this.game.enemySize;
+    this.height = this.game.rows * this.game.enemySize;
+    this.x = 0;
+    this.y = -this.height;
+    this.speedX = 3;
+    this.speedY = 0;
+    this.enemies = [];
+    this.nextWaveTrigger = false;
+    this.create();
   }
   render(context) {
-    if (this.y < 0) this.y += 5
-    this.speedY = 0
+    if (this.y < 0) this.y += 5;
+    this.speedY = 0;
     //context.strokeRect(this.x, this.y, this.width, this.height)
 
     if (this.x < 0 || this.x > this.game.width - this.width) {
-      this.speedX *= -1
-      this.speedY = this.game.enemySize
+      this.speedX *= -1;
+      this.speedY = this.game.enemySize;
     }
-    this.x += this.speedX
-    this.y += this.speedY
+    this.x += this.speedX;
+    this.y += this.speedY;
     this.enemies.forEach((enemy) => {
-      enemy.update(this.x, this.y)
-      enemy.draw(context)
-    })
-    this.enemies = this.enemies.filter((object) => !object.markedForDeletion)
+      enemy.update(this.x, this.y);
+      enemy.draw(context);
+    });
+    this.enemies = this.enemies.filter((object) => !object.markedForDeletion);
   }
   create() {
     for (let y = 0; y < this.game.rows; y++) {
       for (let x = 0; x < this.game.columns; x++) {
-        let enemyX = x * this.game.enemySize
-        let enemyY = y * this.game.enemySize
-        this.enemies.push(new Enemy(this.game, enemyX, enemyY))
+        let enemyX = x * this.game.enemySize;
+        let enemyY = y * this.game.enemySize;
+        this.enemies.push(new Beetlemorph(this.game, enemyX, enemyY));
       }
     }
   }
